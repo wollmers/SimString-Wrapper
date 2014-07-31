a1102bf4b6124d0b363f9587b365ade525fad13c
Helmut Wollmersdorfer <helmut@wollmersdorfer.at>
2014-07-31 19:47:00 +0200

    initial files

---
+++ lib/SimString/Wrapper.pm
@@ -0,0 +1,39 @@
+package SimString::Wrapper;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+SimString::Wrapper - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use SimString::Wrapper;
+
+=head1 DESCRIPTION
+
+SimString::Wrapper is
+
+=head1 AUTHOR
+
+Helmut Wollmersdorfer E<lt>helmut.wollmersdorfer@gmail.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2014- Helmut Wollmersdorfer
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
